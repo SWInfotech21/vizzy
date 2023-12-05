1a41d63b1595086082967183218f28346351d69f
VICTUS\ariful <arifulislam200891@gmail.com>
2023-12-06 00:32:36 +0530

    --update task and client panel

--- public/dmxAppConnect/config.js
+++ public/dmxAppConnect/config.js
@@ -6,5 +6,167 @@ dmx.config({
         "name": "notification"
       }
     ]
+  },
+  "tasks": {
+    "dvTaskList": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "task_list_id"
+        },
+        {
+          "type": "number",
+          "name": "user_id"
+        },
+        {
+          "type": "text",
+          "name": "task_name"
+        },
+        {
+          "type": "text",
+          "name": "required"
+        },
+        {
+          "type": "text",
+          "name": "description"
+        },
+        {
+          "type": "text",
+          "name": "your_notes"
+        },
+        {
+          "type": "text",
+          "name": "status"
+        },
+        {
+          "type": "datetime",
+          "name": "created_on"
+        }
+      ],
+      "outputType": "array"
+    },
+    "ddTasks": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "task_list_id"
+        },
+        {
+          "type": "number",
+          "name": "user_id"
+        },
+        {
+          "type": "text",
+          "name": "task_name"
+        },
+        {
+          "type": "text",
+          "name": "required"
+        },
+        {
+          "type": "text",
+          "name": "description"
+        },
+        {
+          "type": "text",
+          "name": "your_notes"
+        },
+        {
+          "type": "text",
+          "name": "status"
+        },
+        {
+          "type": "datetime",
+          "name": "created_on"
+        }
+      ],
+      "outputType": "array"
+    },
+    "tableRepeat1": {
+      "meta": [
+        {
+          "name": "$index",
+          "type": "number"
+        },
+        {
+          "name": "$key",
+          "type": "text"
+        },
+        {
+          "name": "$value",
+          "type": "object"
+        },
+        {
+          "type": "number",
+          "name": "task_list_id"
+        },
+        {
+          "type": "number",
+          "name": "user_id"
+        },
+        {
+          "type": "text",
+          "name": "task_name"
+        },
+        {
+          "type": "text",
+          "name": "required"
+        },
+        {
+          "type": "text",
+          "name": "description"
+        },
+        {
+          "type": "text",
+          "name": "your_notes"
+        },
+        {
+          "type": "text",
+          "name": "status"
+        },
+        {
+          "type": "datetime",
+          "name": "created_on"
+        }
+      ],
+      "outputType": "array"
+    },
+    "ddTaskList": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "task_list_id"
+        },
+        {
+          "type": "number",
+          "name": "user_id"
+        },
+        {
+          "type": "text",
+          "name": "task_name"
+        },
+        {
+          "type": "text",
+          "name": "required"
+        },
+        {
+          "type": "text",
+          "name": "description"
+        },
+        {
+          "type": "text",
+          "name": "your_notes"
+        },
+        {
+          "type": "text",
+          "name": "status"
+        },
+        {
+          "type": "datetime",
+          "name": "created_on"
+        }
+      ],
+      "outputType": "array"
+    }
   }
 });
